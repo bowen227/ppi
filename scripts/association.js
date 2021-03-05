398a8764c8d37248a072ef6be894612edaea725f
Brent <brent.e.owen@gmail.com>
2021-03-04 22:36:53 -0500

    Added first attempt at auto draft. Working pretty well. May need to tweek to get the teams a bit closer. Largest gap was 6 point.

--- scripts/association.js
+++ scripts/association.js
@@ -4,6 +4,9 @@
 // PPI API ROUTE
 // const PPI_API = 'http://localhost:3000/api'
 
+// ARRAY OF COACHES
+let coaches = []
+
 // ARRAY OF PLAYERS
 let players = []
 let players_group_one = []
@@ -11,14 +14,12 @@ let players_group_two = []
 let players_group_three = []
 let players_group_four = []
 
-// ARRAY OF NAMES TO CREATE PLAYERS FOR PLAYER ARRAY
+// ARRAY OF NAMES TO CREATE PLAYERS AND COACHES
 const firstNames = ['Tom', 'Easton', 'Max', 'Leigh', 'Samuel', 'Doug', 'Peter', 'Bradley', 'Chad', 'Charles']
 const lastNames = ['Owen', 'Golliher', 'Spears', 'Martin', 'Manning', 'Wells', 'Sokohl', 'Reece', 'Smith', 'Coulter']
 const positions = ['Infield', 'Outfield']
 const groups = ['6u', '8u', '10u', '12u']
-
-// TEAM PLAYER POWER INDEX
-let teamPPI = 0
+const type = ['head', 'assistant']
 
 // COACH OBJ
 let coach = {
@@ -38,6 +39,24 @@ let pList = document.getElementById("player-list")
 // SEARCH GROUP
 let searchGroup = 'All'
 
+// CREATE RANDOM COACHES
+function createCoaches() {
+    for (let i = 0; i < 10; i++) {
+        let cIndex = Math.floor(Math.random() * 10)
+        let aIndex = Math.floor(Math.random() * 10)
+
+        const c = {
+            head: {fName: firstNames[cIndex], lName: lastNames[cIndex]},
+            assistant: {fName: firstNames[aIndex], lName: lastNames[aIndex]},
+            teamName: i,
+            players: [],
+            PPI: 0
+        }
+
+        coaches.push(c)
+    }
+}
+
 // CREATE RANDOM PLAYERS
 function createPlayers() {
     for (let index = 0; index < 100 ; index++) {
@@ -52,7 +71,8 @@ function createPlayers() {
             lName: lastNames[lastNameIndex],
             position: positions[pIndex],
             group: groups[groupIndex],
-            PPI: parseFloat((Math.random() / 100 * 5) * 100).toFixed(2),
+            PPI: ((Math.random() / 100 * 5) * 100).toFixed(2),
+            
             profileImg: null
         }
 
@@ -431,5 +451,79 @@ function searchPlayer() {
             </li>
             `
         }
+    }
+}
+
+const one = []
+const two = []
+const three = []
+const four = []
+const five = []
+const selectedCoaches = []
+const selectedPlayers = []
+
+function draftGroupOne() {
+
+    for (let i = 0; i < players_group_one.length; i++) {
+        let p = players_group_one[i]
+        if (p.PPI < 2) {
+            one.push(p)
+        } else if (p.PPI >= 2 && p.PPI < 3) {
+            two.push(p)
+        } else if (p.PPI >= 3 && p.PPI < 4) {
+            three.push(p)
+        } else if (p.PPI >= 4 && p.PPI < 5) {
+            four.push(p)
+        } else {
+            five.push(p)
+        }
+    }
+
+    for (let i = 0; i < Math.floor(players_group_one.length / 10); i++) {
+        console.log(`Number of Teams ${Math.floor(players_group_one.length / 10)}`)
+        selectedCoaches.push(coaches[i])
+    }
+
+    for (let i = 0; i < players_group_one.length; i++) {
+        for (let x = 0; x < selectedCoaches.length; x++) {
+            let c = selectedCoaches[x]
+
+            addPlayerToTeam(c)
+
+        }
+    }
+
+    for (let i = 0; i < selectedCoaches.length; i++) {
+        let element = selectedCoaches[i]
+        const ppis = []
+        element.players.forEach(p => {
+            ppis.push(parseFloat(p.PPI))
+        })
+        const sumPPI = ppis.reduce((a, b) => a + b, 0).toFixed(2)
+        element.PPI = sumPPI
+    }
+}
+
+function addPlayerToTeam(c) {
+    if (five.length > 0) {
+        c.players.push(five[0])
+        selectedPlayers.push(five[0])
+        five.shift()
+    } else if (four.length > 0) {
+        c.players.push(four[0])
+        selectedPlayers.push(four[0])
+        four.shift()
+    } else if (three.length > 0) {
+        c.players.push(three[0])
+        selectedPlayers.push(three[0])
+        three.shift()
+    } else if (two.length > 0) {
+        c.players.push(two[0])
+        selectedPlayers.push(two[0])
+        two.shift()
+    } else if (one.length > 0) {
+        c.players.push(one[0])
+        selectedPlayers.push(one[0])
+        one.shift()
     }
 }
